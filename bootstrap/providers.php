299f8bfd2f732f704c045e3ae9191774ad6811f4
Fahmousss <09031382227177>
2025-10-20 12:00:18 +0700

    feat: Integrate Wirechat messaging system with migrations, configuration, and panel provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\Filament\SupplierPanelProvider::class,
+    App\Providers\Wirechat\ChatsPanelProvider::class,
 ];
